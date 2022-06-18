aa27f5ce498a3a362086c05cd591b9f43ff75e87
Chijioke Chika <8pluscreative@gmail.com>
2022-06-17 23:42:05 -0400

    Redux

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,16 +1,27 @@
-import { createApi, fetchBaseQuery } from './@redux/toolkit/query';
+import { createApi, fetchBaseQuery } from './@redux/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Key': 'b0834bc328msh0d81b6138d53f5ep1bdadejsn4c1a3d09b114',
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
 export const cryptoApi = createApi({
 	reducerPath: 'cryptoApi',
-	baseQuery: fetchBaseQuery({ baseUrl })
+	baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
 })
+
+export const {
+	useGetCryptosQuery,
+} = cryptoApi;
 
 
 // const options = {
